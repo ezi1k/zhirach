96e9547b840cd48633597571c54c5e08ccd87422
subo-bratiik <saharovandrej200@gmail.com>
2026-04-13 18:57:38 +0300

    add zinit shit ZOV

---
+++ zinit/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+    char *argv[] = {"sh", "-c", "/etc/zinit/enabled/*", NULL};
+    char *envp[] = {NULL};
+
+int main()
+{
+  pid_t pid1 = getpid();
+  //printf("%d\n", pid1);
+  fork();
+  pid_t pid2 = getpid();
+  //printf("%d\n", pid2);
+  if (pid1 == pid2)
+  {
+  }
+  else
+  {
+  execve("/bin/sh", argv, envp);
+  }
+  printf("done")
+    while (true) {sleep(120); }
+}
