6d998ab869330c02ddcfcb78349bd96aaf3ec4f1
subo-bratiik <saharovandrej200@gmail.com>
2026-04-15 18:05:47 +0300

    dcpcd > dhcpcd

--- zservice/dcpcd.sh
+++ zservice/dcpcd.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-SERVICES="/sbin/resolvd /sbin/dcpcd"
+SERVICES="/sbin/resolvd /sbin/dhcpcd"
 
 trap 'kill 0; exit' INT TERM
 
 for cmd in $SERVICES; do
-    (
-        while true; do
-            $cmd
-            sleep 1
-        done
-    ) &
+  (
+    while true; do
+      $cmd
+      sleep 1
+    done
+  ) &
 done
 
 wait
-
